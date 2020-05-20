6daebed3ff568a386aaa4ee25321cd49cd887313
Milan-CiudadReal <61572718+Milan-CiudadReal@users.noreply.github.com>
2020-05-20 01:47:37 -0400

    Delete API_Keys.py

--- API_Keys.py
+++
@@ -1,5 +0,0 @@
-API_key = "qtYX8mxTdx4QLXX0zDYhMb73l"
-secret_key = "o8PGtmDS5r2tmTxbUpp6D2MfE2fkSQCqIOjGwaXo063wF5vwn8"
-access_token = "1259716202630430720-wjCQGdVGZziVjtgzBT9LaRh9KgtL55"
-secret_access_token = "FGmH7Bb5FNYk6YCMcMi3DUwZudYfzlN8uz23uBo3SU82V"
-print("imported")
